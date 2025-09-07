2327ced438117664753a7d07b350ec4be0eae7b4
naman-1905 <namanoj19@gmail.com>
2025-09-07 17:43:14 +0530

    Created a Dashboard Component

---
+++ app/screens/DashboardLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardLayout() {
+  return (
+    <div className='text-black'>
+      /app/screens/DashboardLayout.js
+    </div>
+  )
+}
+
+export default DashboardLayout
