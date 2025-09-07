08a083d27d303497456c7c3d1049aaa1a298d3f5
naman-1905 <namanoj19@gmail.com>
2025-09-07 17:43:33 +0530

    Created a Expense Component

---
+++ app/screens/IncomeLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IncomeLayout() {
+  return (
+    <div className='text-black'>
+      /app/screens/IncomeLayout.js
+    </div>
+  )
+}
+
+export default IncomeLayout
