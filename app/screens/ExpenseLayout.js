4b2754eb0b13b7ee37e186d0ce2f8c962fbe45bb
naman-1905 <namanoj19@gmail.com>
2025-09-07 17:43:23 +0530

    Created a Expense Component

---
+++ app/screens/ExpenseLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseLayout() {
+  return (
+    <div className='text-black'>
+      /app/screens/ExpenseLayout.js
+    </div>
+  )
+}
+
+export default ExpenseLayout
