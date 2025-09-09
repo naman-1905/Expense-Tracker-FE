1f78493733c83ccab251da29ae0144f9b1426bb8
naman-1905 <namanoj19@gmail.com>
2025-09-09 20:04:36 +0530

    Added cards to the Income Layout

--- app/screens/IncomeLayout.js
+++ app/screens/IncomeLayout.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import IncomeChartCard from '../cards/IncomeChartCard'
+import IncomeSourcesCard from '../cards/IncomeSourcesCard'
 
 function IncomeLayout() {
   return (
-    <div className='text-black'>
-      /app/screens/IncomeLayout.js
+    <div className="flex gap-4 text-black">
+      <div className="flex-1">
+        <IncomeChartCard />
+      </div>
+      <div className="flex-1">
+        <IncomeSourcesCard />
+      </div>
     </div>
   )
 }
