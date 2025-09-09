05ce2af3c6da5dd1671b75c96686c98b12fb2096
naman-1905 <namanoj19@gmail.com>
2025-09-09 18:42:55 +0530

    Aligned the cards

--- app/screens/DashboardLayout.js
+++ app/screens/DashboardLayout.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Summary from '../cards/Summary'
+import FinanceOverview from '../cards/FinanceOverview'
+import TransactionCard from '../cards/TransactionCard'
 
 function DashboardLayout() {
   return (
     <div className='text-black'>
-      /app/screens/DashboardLayout.js
+      <Summary/>
+      <div className='mt-8 grid grid-cols-1 md:grid-cols-2 gap-6'>
+        <FinanceOverview/>
+        <TransactionCard/>
+      </div>
     </div>
   )
 }
