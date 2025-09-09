4f925b79e078801d6850263e4ae80035e4c85614
naman-1905 <namanoj19@gmail.com>
2025-09-09 20:31:21 +0530

    Imported Expenses Card in Layout.js

--- app/screens/ExpenseLayout.js
+++ app/screens/ExpenseLayout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ExpenseChartCard from '../cards/ExpenseChartCard'
 
 function ExpenseLayout() {
   return (
     <div className='text-black'>
-      /app/screens/ExpenseLayout.js
+     <ExpenseChartCard/>
     </div>
   )
 }
