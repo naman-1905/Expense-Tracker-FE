5db24889b2e636407e1440814fce21a166a9e839
naman-1905 <namanoj19@gmail.com>
2025-09-09 21:01:14 +0530

    Prepared the Expense Layout

--- app/screens/ExpenseLayout.js
+++ app/screens/ExpenseLayout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ExpenseChartCard from '../cards/ExpenseChartCard'
+import ExpensesCard from '../cards/ExpenseSourcesCard'
 
 function ExpenseLayout() {
   return (
-    <div className='text-black'>
-     <ExpenseChartCard/>
+    <div className="text-black space-y-6">
+      <ExpenseChartCard />
+      <ExpensesCard />
     </div>
   )
 }
