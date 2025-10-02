0034293156914ad406ed8171fd0672e861199f80
naman-1905 <namanoj19@gmail.com>
2025-10-02 13:12:56 +0000

    Updated responsiveness for Expenses Chart

--- app/screens/ExpenseLayout.js
+++ app/screens/ExpenseLayout.js
@@ -4,9 +4,11 @@ import ExpensesCard from '../cards/ExpenseSourcesCard'
 
 function ExpenseLayout() {
   return (
-    <div className="text-black space-y-6">
-      <ExpenseChartCard />
-      <ExpensesCard />
+    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="text-black space-y-6 py-6">
+        <ExpenseChartCard />
+        <ExpensesCard />
+      </div>
     </div>
   )
 }
