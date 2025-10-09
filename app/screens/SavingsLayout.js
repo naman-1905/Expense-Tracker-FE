87cb4032fafef4c252a961fde12d1ab56d052073
naman-1905 <namanoj19@gmail.com>
2025-10-09 20:23:20 +0000

    Created a Savings Layout for the Piggy Bank Menu

---
+++ app/screens/SavingsLayout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SavingsLayout() {
+  return (
+<div>
+  <p>In Progress for the next production version :)</p>
+  <br/>
+  <p>Regards,</p>
+  <p>Naman :)</p>
+</div>
+  )
+}
+
+export default SavingsLayout
